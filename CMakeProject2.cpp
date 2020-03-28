66823b966b3163c640d86068670875c72e59c60a
yueer18 <475065288@qq.com>
2020-03-28 20:53:48 +0800

    init

--- CMakeProject2.cpp
+++ CMakeProject2.cpp
@@ -1,12 +1,80 @@
-﻿// CMakeProject2.cpp: 定义应用程序的入口点。
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
 //
 
 #include "CMakeProject2.h"
-
+#include <iomanip>
+#include <vector>
+#include <iostream>
 using namespace std;
+
+#if 0
+class SavingsAccount {
+private:
+	int id;        //账户
+	double balance; //余额
+	double rate;     //存款的年利率
+	int lastDate;    //最后更新日期
+	double accumlation; //余额日累加和
+
+	//记录一笔账，data为日期，amount为金额，desc为说明
+	void record(int data, double amount);
+	//获得指定日期为止的存款金额按日累计值
+	double accumulate(int data) const {
+		return accumlation + balance * (data - lastDate);
+	}
+public:
+	//构造函数
+	SavingsAccount(int data, int id, double rate);
+	int getId() { return id; }
+	double getBalance() { return balance; }
+	double getRate() { return rate; }
+
+	//存
+	void deposit(int data, double amount);
+	//取
+	void withdraw(int date, double amount);
+	//节算利息
+	void settle(int date);
+	//显示
+	void show();
+};
+
+SavingsAccount::SavingsAccount(int date, int id, double rate)
+	:id(id), balance(0), rate(rate), lastDate(date), accumlation(0) {
+	cout << date << "\t#" << id << "is create" << endl;
+}
+void SavingsAccount::record(int date, double amount) {
+
+}
+
+void SavingsAccount::record(int date, double amount) {
+
+}
+#endif
+
+
+void test_167()
+{
+	vector<int> arr = { 1,2,3,4,6,55 };
+	Answer A(arr);
+
+	A.show();
+	for (auto e : A.twoSum(arr, 5)) {
+		cout << e << endl;
+	}
+}
+
+void test_633() 
+{
+	Answer A(vector<int>{});
+	cout << A.judgeSquareSum(4) << endl;
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+//	test_167();
+	test_633();
+
 	return 0;
 }
+ 
--- CMakeProject2.h
+++ CMakeProject2.h
@@ -1,8 +1,9 @@
-﻿// CMakeProject2.h: 标准系统包含文件的包含文件
+﻿// CMakeProject1.h: 标准系统包含文件的包含文件
 // 或项目特定的包含文件。
 
 #pragma once
 
-#include <iostream>
+#include "answer/DoublePointer.h"
+
 
 // TODO: 在此处引用程序需要的其他标头。
